9e300d96c9f8e71c2669fd5d2386a9e87e8ef24f
Samuel de Beer <dbrsam003@myuct.ac.za>
2019-05-11 22:10:45 +0200

    file reading and writing works

--- Audio.h
+++ Audio.h
@@ -17,19 +17,111 @@ template <typename T, int numchan>
 	private:
 	T bitnum;	//either 8 or 16
 	int fs;    //sampling freq
-	std::vector<T> monovec;
+	int audiosize; // size of file
+	int channelnum;
+	int samplenum;
+	std::vector<T> audiovec;
 	
 	
 	
 	
 	public:
 	
-	Audio();
-	Audio(T bitnumber, int samplef) : bitnum(bitnumber), fs(samplef){}
+	Audio();  //default constructor
+	Audio(T bitnumber, int samplef) : bitnum(bitnumber), fs(samplef), channelnum(numchan){}
 	
 	
-	load(std::string file);
-	save(std::string file);
+	//copy constructor
+	Audio(const Audio & rhs) : bitnum(rhs.bitnum), fs(rhs.fs), channelnum(rhs.channelnum), samplenum(rhs.samplenum){
+		
+		audiovec = rhs.audiovec;
+		
+	}
+	
+	//copy assignment
+	Audio operator=(const Audio & rhs)
+		{
+		channelnum = rhs.channelnum;
+		fs = rhs.fs;
+		bitnum = rhs.bitnum;
+		samplenum = rhs.samplenum;
+		audiosize = rhs.audiosize;
+		audiovec = rhs.audiovec;
+		return *this;
+			}
+			
+	//move constructor
+	Audio(Audio && rhs) : channelnum(rhs.channelnum),fs(rhs.fs),samplenum(rhs.samplenum), bitnum(rhs.bitnum), audiosize(rhs.audiosize)
+		{
+		this->data = rhs.data;
+		rhs.channelnum = -1;
+		rhs.fs = -1;
+		rhs.samplenum = -1;
+		rhs.bitnum = -1;
+		rhs.audiosize = -1;
+		}
+	//move assignment
+	Audio operator=(Audio && rhs){
+		channelnum = rhs.channelnum;	rhs.channelnum = -1;
+		fs = rhs.fs;					rhs.fs = -1;
+		samplenum = rhs.samplenum;		rhs.samplenum = -1;
+		bitnum = rhs.bitnum;			rhs.bitnum = -1;
+		audiosize = rhs.audiosize;		rhs.audiosize = -1;
+		return *this;
+	}
+	
+
+	//destructor
+	~Audio(){};
+	
+	void load(std::string file){
+		std::ifstream inputfile;
+				
+		inputfile.open(file, std::ios::binary);
+				
+		//finding size of file
+		inputfile.seekg(0,inputfile.end); //go to end of stream
+		 audiosize = (int)(inputfile.tellg()); // get size
+		inputfile.seekg(0, inputfile.beg);  // go back to beginning
+
+		//determining number of samples
+		samplenum = audiosize/channelnum;
+		
+
+		char * temp_array = new char[samplenum];
+		inputfile.read(temp_array, samplenum);
+		for(int g = 0; g < samplenum; g++)
+		{
+			audiovec.push_back((T)(temp_array[g]));
+			}
+		delete temp_array;
+		inputfile.close();
+				
+		
+	}
+	void save(std::string file){
+		
+		
+		std::ofstream outputfile(file, std::ios::binary);
+				//creating temporary array
+		char * save_arr = new char[samplenum];
+		for(int k = 0; k < samplenum; ++k)
+		{
+			save_arr[k] = (char)audiovec[k];
+		}
+		//write temporary array contents to output file
+		outputfile.write(save_arr, samplenum);
+		delete save_arr;
+		outputfile.close();
+		}
+		
+		
+	Audio add(const Audio & other){
+		
+	}
+		
+		
+	
 };
 
 
@@ -40,9 +132,11 @@ template <typename T>
 	
 	private:
 	T bitnum;	//either 8 or 16
+	int channelnum;
 	int fs;    //sampling freq
-	
-	std::vector<std::pair<T,T>>  stereovec;
+	int audiosize;
+	int samplenum; 
+	std::vector<std::pair<T,T>>  audiovec;
 	
 	
 	
@@ -50,11 +144,100 @@ template <typename T>
 	
 	Audio();
 	
-	Audio(T bitnumber,int samplef, std::vector<std::pair<T,T>> stereo); //stereo constructor
+	Audio(T bitnumber,int samplef) : bitnum(bitnumber), fs(samplef), channelnum(2){} //stereo constructor
+	//copy constructor
+	Audio(const Audio & rhs) : bitnum(rhs.bitnum), fs(rhs.fs), channelnum(rhs.channelnum), samplenum(rhs.samplenum){
+		
+		audiovec = rhs.audiovec;
+		
+	}
+	
+	//copy assignment
+	Audio operator=(const Audio & rhs)
+		{
+		channelnum = rhs.channelnum;
+		fs = rhs.fs;
+		bitnum = rhs.bitnum;
+		samplenum = rhs.samplenum;
+		audiosize = rhs.audiosize;
+		audiovec = rhs.audiovec;
+		return *this;
+			}
+			
+	//move constructor
+	Audio(Audio && rhs) : channelnum(rhs.channelnum),fs(rhs.fs),samplenum(rhs.samplenum), bitnum(rhs.bitnum), audiosize(rhs.audiosize)
+		{
+		this->data = rhs.data;
+		rhs.channelnum = -1;
+		rhs.fs = -1;
+		rhs.samplenum = -1;
+		rhs.bitnum = -1;
+		rhs.audiosize = -1;
+		}
+	//move assignment
+	Audio operator=(Audio && rhs){
+		channelnum = rhs.channelnum;	rhs.channelnum = -1;
+		fs = rhs.fs;					rhs.fs = -1;
+		samplenum = rhs.samplenum;		rhs.samplenum = -1;
+		bitnum = rhs.bitnum;			rhs.bitnum = -1;
+		audiosize = rhs.audiosize;		rhs.audiosize = -1;
+		return *this;
+	}
+	
+
+	//destructor
+	~Audio(){};
 	
 	
-	load(std::string file);
-	save(std::string file);
+	void load(std::string file){
+		
+		std::ifstream inputfile;
+				
+		inputfile.open(file, std::ios::binary);
+				
+		//finding size of file
+		inputfile.seekg(0,inputfile.end); //go to end of stream
+		audiosize = (int)(inputfile.tellg()); // get size
+		inputfile.seekg(0, inputfile.beg);  // go back to beginning
+
+		//determining number of samples
+		samplenum = audiosize/channelnum;
+		
+
+		char * temp_array = new char[2*samplenum];
+		inputfile.read(temp_array, samplenum);
+		
+		for(int g = 0; g < samplenum*2; g=g+2)
+		{
+			std::pair<T,T> stereopair = std::make_pair((T)temp_array[g],(T)temp_array[g+1]);
+			audiovec.push_back(stereopair);
+			}
+		delete temp_array;
+		inputfile.close();
+		
+		
+	}
+	void save(std::string file){
+		
+		std::ofstream outputfile(file, std::ios::binary);
+				//creating temporary array
+		char * save_arr = new char[samplenum*2];
+		int count = 0;
+		int k = 0;
+		while (k<samplenum)
+		{
+			save_arr[count] = (char)audiovec[k].first;
+			save_arr[count+1] = (char)audiovec[k].second;
+			count = count+2;
+			k++;
+		}
+		//write temporary array contents to output file
+		outputfile.write(save_arr, samplenum);
+		delete save_arr;
+		outputfile.close();
+		
+		
+	}
 };
 
 
--- Driver.cpp
+++ Driver.cpp
@@ -6,43 +6,95 @@ using namespace std;
 int main(int argc, char *argv[])
 {
 	
-	//ADD
-	if (argc == 12 && string(argv[9]) == "-add"){  //check if add command
-		cout << "Adding" << endl;
-		int channelnum;
+	int channelnum;
 		int bitsize;
+		int samplefreq;
 		stringstream sstream;
 		
 		
 		//getting number of channels
 		sstream << string(argv[6]);
 		sstream >> channelnum;
+		sstream.clear();
 		
 		//getting bit sample size
 		sstream << string(argv[4]);
-		sstream >> bitsize
+		sstream >> bitsize;
+		sstream.clear();
+		
+		//getting sampling frequency
+		sstream << string(argv[2]);
+		sstream >> samplefreq;
+		sstream.clear();
 		
 		
+		cout << bitsize << " bit sample size" << endl;
+		cout << samplefreq << " Hz sampling frequency" <<endl;
+	
+	//ADD
+	if (argc == 12 && string(argv[9]) == "-add"){  //check if add command
+		cout << "Adding" << endl;
+		//two Audio objects
+		
+		
+
 		if(channelnum ==1){   // mono file
 			cout<< "Mono" << endl;
 			
-			if(bitsize == 8){
+			if(bitsize == 8){   //8 bit sample size
+				DBRSAM003::Audio<int8_t,1> audio1(bitsize, samplefreq);
+				DBRSAM003::Audio<int8_t,1> audio2(bitsize, samplefreq);
+				
+				//loading files
+				audio1.load(string(argv[10]));
+				audio2.load(string(argv[11]));
+				//save output
+			//	audio3.save(string(argv[8]));
 				
 			}
 			
-			
-			
+			else if(bitsize == 16){ //16 bit sample size
+				DBRSAM003::Audio<int16_t,1> audio1(bitsize, samplefreq);
+				DBRSAM003::Audio<int16_t,1> audio2(bitsize, samplefreq);
+				
+				//loading files
+				audio1.load(string(argv[10]));
+				audio2.load(string(argv[11]));
+				//save output
+				audio1.save(string(argv[8]));
+			}
 		}
 		
-		
-		
-		
-		else if(channelnum ==2)   //stereo file
-		cout << "Stereo" << endl;
-		
-		
-		
-		
+		else if(channelnum ==2){   //stereo file
+			cout << "Stereo" << endl;
+			if(bitsize == 8){ //8 bit sample size
+				DBRSAM003::Audio<int8_t,2> audio1(bitsize, samplefreq);
+				DBRSAM003::Audio<int8_t,2> audio2(bitsize, samplefreq);
+				
+				//loading files
+				audio1.load(string(argv[10]));
+				audio2.load(string(argv[11]));
+				//save output
+				audio1.save(string(argv[8]));
+			}
+			
+			else if(bitsize == 16){ //16 bit sample size
+				DBRSAM003::Audio<int16_t,2> audio1(bitsize, samplefreq);
+				DBRSAM003::Audio<int16_t,2> audio2(bitsize, samplefreq);
+				
+				//loading files
+				audio1.load(string(argv[10]));
+				audio2.load(string(argv[11]));
+				//save output
+				audio1.save(string(argv[8]));
+			}
+	}
+	//END OF ADD
+
+	
+	
+	
+	
 	}
 	
 	
@@ -56,4 +108,5 @@ int main(int argc, char *argv[])
 	
 	
 	
-}
+
+} 
