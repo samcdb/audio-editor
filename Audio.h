9dde663497d42dc64da68aed4acab015258dedc9
Samuel de Beer <dbrsam003@myuct.ac.za>
2019-05-11 18:27:51 +0200

    Doing add

--- Audio.h
+++ Audio.h
@@ -1,16 +1,65 @@
 #ifndef Audio_h
 #define Audio_h
+#include <cstdint>
+#include <vector>
+#include <utility>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+namespace DBRSAM003{
+
+
+template <typename T, int numchan>
+ class Audio{
+	
+	private:
+	T bitnum;	//either 8 or 16
+	int fs;    //sampling freq
+	std::vector<T> monovec;
+	
+	
+	
+	
+	public:
+	
+	Audio();
+	Audio(T bitnumber, int samplef) : bitnum(bitnumber), fs(samplef){}
+	
+	
+	load(std::string file);
+	save(std::string file);
+};
 
 
 
 
+template <typename T>
+ class Audio<T,2>{
+	
+	private:
+	T bitnum;	//either 8 or 16
+	int fs;    //sampling freq
+	
+	std::vector<std::pair<T,T>>  stereovec;
+	
+	
+	
+	public:
+	
+	Audio();
+	
+	Audio(T bitnumber,int samplef, std::vector<std::pair<T,T>> stereo); //stereo constructor
+	
+	
+	load(std::string file);
+	save(std::string file);
+};
 
 
 
-
-
-
-
+}
 
 
 #endif
--- Driver.cpp
+++ Driver.cpp
@@ -1,1 +1,59 @@
 #include "Audio.h"
+
+using namespace std;
+
+
+int main(int argc, char *argv[])
+{
+	
+	//ADD
+	if (argc == 12 && string(argv[9]) == "-add"){  //check if add command
+		cout << "Adding" << endl;
+		int channelnum;
+		int bitsize;
+		stringstream sstream;
+		
+		
+		//getting number of channels
+		sstream << string(argv[6]);
+		sstream >> channelnum;
+		
+		//getting bit sample size
+		sstream << string(argv[4]);
+		sstream >> bitsize
+		
+		
+		if(channelnum ==1){   // mono file
+			cout<< "Mono" << endl;
+			
+			if(bitsize == 8){
+				
+			}
+			
+			
+			
+		}
+		
+		
+		
+		
+		else if(channelnum ==2)   //stereo file
+		cout << "Stereo" << endl;
+		
+		
+		
+		
+	}
+	
+	
+	else if (argc == 5 && string(argv[9]) == "add" ){ 	//check if add command
+		cout << "Adding" << endl;
+		
+		
+	}
+	
+	
+	
+	
+	
+}
