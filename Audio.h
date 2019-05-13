e53ae46a576ad6c123c540fb28cede9971dd4752
Samuel de Beer <dbrsam003@myuct.ac.za>
2019-05-13 04:40:54 +0200

    trying to get range right

--- Audio.h
+++ Audio.h
@@ -1,5 +1,6 @@
 #ifndef Audio_h
 #define Audio_h
+#include "normalise.h"
 #include <cstdint>
 #include <vector>
 #include <utility>
@@ -8,10 +9,13 @@
 #include <fstream>
 #include <sstream>
 #include <cmath>
+#include <algorithm>
+#include <numeric>
+
 namespace DBRSAM003{
 
 
-template <typename T, int numchan>
+template <typename T, int numchan=1>
  class Audio{
 	
 	private:
@@ -85,36 +89,73 @@ template <typename T, int numchan>
 		inputfile.open(file, std::ios::binary);
 				
 		//finding size of file
+		/*
 		inputfile.seekg(0,inputfile.end); //go to end of stream
 		 audiosize = (int)(inputfile.tellg()); // get size
 		inputfile.seekg(0, inputfile.beg);  // go back to beginning
-
+		std::cout << audiosize<< std::endl;
 		//determining number of samples
-		samplenum = audiosize/channelnum;
-		std::cout << samplenum<< std::endl;
-
+		samplenum = (audiosize/(sizeof(T)*1) );
+		std::cout << "sample num " <<samplenum<< std::endl;
+		
+		
+		
+		
+		
+		std::cout << samplenum << std::endl;
 		char * temp_array = new char[samplenum];
 		inputfile.read(temp_array, samplenum);
+		audiovec.resize(samplenum);
+		*/
+		
+		
+		inputfile.seekg(0, inputfile.end);
+		std::streampos fileSize = inputfile.tellg();
+
+		samplenum = fileSize / (sizeof(T));
+		audiovec.resize(samplenum);
+		int fLength = samplenum / (float)fs;
+
+		inputfile.seekg(0, inputfile.beg);
+		std::cout << "sample num " <<samplenum<< std::endl;
+		inputfile.read((char *)& audiovec[0], samplenum);
+		
+		/*
 		for(int g = 0; g < samplenum; g++)
 		{
-			audiovec.push_back((T)(temp_array[g]));
+			audiovec[g] = (T)(temp_array[g]);
 			}
 		delete temp_array;
+		std::cout << "last in array " << audiovec[audiovec.size()-1] <<std::endl;
 		inputfile.close();
-				
+				*/
 		
 	}
 	void save(std::string file){
 		std::ofstream outputfile(file, std::ios::binary);
 				//creating temporary array
+				/*
 		char * save_arr = new char[samplenum];
+		int counter = 0;
 		for(int k = 0; k < samplenum; ++k)
 		{
 			save_arr[k] = (char)audiovec[k];
+			counter++;
 		}
+		std::cout << "last in array " << save_arr[audiovec.size()-1] <<std::endl;
+
+		std::cout << "save length " << counter <<std::endl;
 		//write temporary array contents to output file
 		outputfile.write(save_arr, samplenum);
 		delete save_arr;
+		*/
+		
+		
+		std::cout << "vec sizeeeee " << audiovec.size() <<std::endl;
+
+		outputfile.write((char *)& audiovec[0], audiovec.size());
+		outputfile.flush();
+		
 		outputfile.close();
 		}
 		
@@ -124,7 +165,7 @@ template <typename T, int numchan>
 		
 		auto beg1 = answer.audiovec.begin(), end1 = answer.audiovec.end();
 		auto beg2 = other.audiovec.begin(), end2 = other.audiovec.end();
-
+	int counter = 0;
 		while(beg1 != end1){
 	//	for(int j=0;j<answer.getAudiovec().size();++j){
 			int sum = *beg1 + *beg2;
@@ -139,7 +180,15 @@ template <typename T, int numchan>
 			}
 			
 			++beg1; ++beg2;
+			++counter;
 		}
+		std::cout << "vec size " <<answer.audiovec.size() << std::endl;
+		std::cout << "count size " <<counter << std::endl;
+		std::cout << "last two vector elements " <<answer.audiovec[audiovec.size()-30] << " " <<answer.audiovec[audiovec.size()-10] << std::endl;
+		
+		
+		
+		
 		return answer;
 	}
 	
@@ -168,7 +217,7 @@ template <typename T, int numchan>
 			
 			++cutend; ++beg2; 
 		}
-		std::cout <<"hi"<<std::endl;
+		
 		answer.audiovec.clear();
 		answer.audiovec = newaudiovec;
 		answer.samplenum = newaudiovec.size();
@@ -177,7 +226,109 @@ template <typename T, int numchan>
 		
 	}
 	
+	Audio concatenate(Audio & other){
+		Audio answer(*this);
+		int joinpoint = answer.samplenum;
+		answer.samplenum += other.samplenum;
+		answer.audiovec.resize(answer.samplenum);
+		
+		auto beg1 = answer.audiovec.begin() + joinpoint, end1 = answer.audiovec.end();
+		auto beg2 = other.audiovec.begin(), end2 = other.audiovec.end();
+		
+		while(beg1 != end1){
+			
+			*beg1 = *beg2;
+			
+			++beg1; ++beg2;
+			
+		}
+		
+		return answer;
+		
+	}
 	
+	//ranged add
+	
+	Audio radd(std::pair<int,int> rangepair1,std::pair<int,int> rangepair2, Audio & other){
+		Audio answer(*this);
+		Audio oth(other);
+		std::vector<T>  ansraddaudiovec;
+		std::vector<T>  othraddaudiovec;
+		ansraddaudiovec.resize((rangepair1.second - rangepair1.first)*fs);
+		othraddaudiovec.resize((rangepair2.second - rangepair2.first)*fs);
+		std::cout << other.audiovec.size()<< std::endl;
+		std::copy ( answer.audiovec.begin()+rangepair1.first*fs, answer.audiovec.begin()+rangepair1.second*fs, ansraddaudiovec.begin() );   // copy range into new vector
+		std::cout << "end of radd" << std::endl;
+		std::copy ( oth.audiovec.begin()+rangepair2.first*fs, oth.audiovec.begin()+rangepair2.second*fs, othraddaudiovec.begin() );	//copy range into new vector
+		
+		answer.audiovec = ansraddaudiovec;
+		oth.audiovec = othraddaudiovec;
+		
+		answer = answer + oth;
+		answer.samplenum = answer.audiovec.size();
+		
+		return answer;
+	
+	}
+	
+	//volume
+	Audio volume(std::pair<float, float> setting){
+		Audio volchange(*this);
+		
+		auto beg1 = volchange.audiovec.begin(), end1 = volchange.audiovec.end();
+		
+		while(beg1 != end1){
+	
+			float volume = *beg1 *setting.first;
+			if (volume > std::pow(2,bitnum-1)-1){
+				*beg1 = (std::pow(2,bitnum-1)-1);
+			}
+			else if(volume < -1*std::pow(2,bitnum-1)-1){
+				*beg1 = -1*(std::pow(2,bitnum-1)-1);
+			}
+			else{
+				*beg1 = volume;
+			}
+			
+			++beg1;
+		}
+		return volchange;
+	}
+	
+	//reverse
+	Audio reverse(){
+		Audio rev(*this);
+		std::reverse(rev.audiovec.begin(),rev.audiovec.end());
+		
+		return rev;
+	}
+	
+	//RMS
+	float rms(){
+		int accsum = 0;
+		
+		accsum = std::accumulate(audiovec.begin(), audiovec.end(), accsum, [](int a, T b) {
+			
+			return a + (b*b);
+			
+		});
+
+		float rmsanswer = sqrt(accsum / (float)samplenum);
+
+		return rmsanswer;
+		
+	}
+	
+	
+	//normalisation
+	Audio normalisation(std::pair<float, float> givenrms) {
+		float nowrms = rms();
+
+		Audio answer(bitnum, fs);
+		answer.samplenum = samplenum;
+		std::transform(audiovec.begin(), audiovec.end(), answer.audiovec.begin(), normalise<T>(givenrms.first, nowrms, bitnum));
+		return answer;
+	}
 	
 	//overloaded operators
 	//add
@@ -187,6 +338,14 @@ template <typename T, int numchan>
 	//cut
 	Audio operator^ (std::pair<int,int> cutrange){
 	return cut(cutrange);
+	}
+	//concatenate
+	Audio operator|( Audio & other){
+	return concatenate(other);
+	}
+	//volume
+	Audio operator* (std::pair<float,float> setting){
+		return volume(setting);
 	}
 		
 	
@@ -262,7 +421,7 @@ template <typename T>
 	}
 	
 	void load(std::string file){
-		
+		std::cout <<"Loading" <<std::endl;
 		std::ifstream inputfile;
 				
 		inputfile.open(file, std::ios::binary);
@@ -274,40 +433,46 @@ template <typename T>
 
 		//determining number of samples
 		samplenum = audiosize/channelnum;
+		std::cout << "channel " << channelnum <<std::endl;
 		
 
 		char * temp_array = new char[2*samplenum];
-		inputfile.read(temp_array, samplenum);
+		inputfile.read(temp_array, 2*samplenum);
 		
 		for(int g = 0; g < samplenum*2; g=g+2)
 		{
 			std::pair<T,T> stereopair = std::make_pair((T)temp_array[g],(T)temp_array[g+1]);
 			audiovec.push_back(stereopair);
+			
 			}
+		
 		delete temp_array;
 		inputfile.close();
 		
 		
 	}
 	void save(std::string file){
-		
+		std::cout <<"saving" <<std::endl;
 		std::ofstream outputfile(file, std::ios::binary);
 				//creating temporary array
 		char * save_arr = new char[samplenum*2];
 		int count = 0;
 		int k = 0;
+		std::cout << "hi" << std::endl;
 		while (k<samplenum)
 		{
+			
 			save_arr[count] = (char)audiovec[k].first;
 			save_arr[count+1] = (char)audiovec[k].second;
 			count = count+2;
 			k++;
 		}
+		std::cout << count << std::endl;
 		//write temporary array contents to output file
-		outputfile.write(save_arr, samplenum);
+		
+		outputfile.write(save_arr, samplenum*2);
 		delete save_arr;
 		outputfile.close();
-		
 		
 	}
 	
@@ -351,12 +516,198 @@ template <typename T>
 	}
 	
 	
+		Audio cut(std::pair<int,int> cutrange){
+		Audio answer(*this);
+		std::vector<std::pair<T,T>> newaudiovec;
+		
+		int startsmpl = cutrange.first*fs*2; //multiply by sampling freq        ************establish why
+		int endsmpl = cutrange.second*fs*2;
+		newaudiovec.resize(samplenum - (endsmpl-startsmpl) + 1);
+		std::cout << "secs " <<(samplenum )*1/fs <<std::endl;
+		auto beg1 = answer.audiovec.begin(), end1 = answer.audiovec.end();    
+		auto beg2 = newaudiovec.begin(), end2 = newaudiovec.end();
+		auto cutstart = beg1+startsmpl+1, cutend = beg1+endsmpl;
+		std::cout<<"new "<<newaudiovec.size()<<std::endl;
+		std::cout <<"old " <<this->audiovec.size() <<std::endl;
+		std::cout<<"start " <<startsmpl << " end " << endsmpl<<std::endl;
+		while(beg1 != cutstart){  //go from beginning to start of cut
+			*beg2 = *beg1;
+			
+			++beg1; ++beg2;
+		}
+			
+		
+		while(cutend != end1 ){  //go from end of cut to end
+			*beg2 = *cutend;
+			
+			++cutend; ++beg2; 
+		}
+		
+		answer.audiovec.clear();
+		answer.audiovec = newaudiovec;
+		answer.samplenum = newaudiovec.size();
+		
+		return answer;
+		
+	}
+	//concat
+	Audio concatenate(Audio & other){
+		Audio answer(*this);
+		int joinpoint = answer.samplenum;
+		answer.samplenum += other.samplenum;
+		answer.audiovec.resize(answer.samplenum);
+		
+		auto beg1 = answer.audiovec.begin() + joinpoint, end1 = answer.audiovec.end();
+		auto beg2 = other.audiovec.begin(), end2 = other.audiovec.end();
+		
+		while(beg1 != end1){
+			
+			*beg1 = *beg2;
+			
+			++beg1; ++beg2;
+			
+		}
+		
+		return answer;
+		
+	}
+	
+	//ranged add
+	
+	Audio radd(std::pair<int,int> rangepair1,std::pair<int,int> rangepair2, Audio & other){
+		Audio answer(*this);
+		Audio oth(other);
+		std::vector<std::pair<T,T>>  ansraddaudiovec;
+		std::vector<std::pair<T,T>>  othraddaudiovec;
+		ansraddaudiovec.resize((rangepair1.second - rangepair1.first)*fs*2);
+		othraddaudiovec.resize((rangepair2.second - rangepair2.first)*fs*2);
+		std::cout << other.audiovec.size()<< std::endl;
+		std::copy ( answer.audiovec.begin()+rangepair1.first*fs*2, answer.audiovec.begin()+rangepair1.second*fs*2, ansraddaudiovec.begin() );   // copy range into new vector
+		std::cout << "end of radd" << std::endl;
+		std::copy ( oth.audiovec.begin()+rangepair2.first*fs*2, oth.audiovec.begin()+rangepair2.second*fs*2, othraddaudiovec.begin() );	//copy range into new vector
+		
+		answer.audiovec = ansraddaudiovec;
+		oth.audiovec = othraddaudiovec;
+		
+		answer = answer + oth;
+		answer.samplenum = answer.audiovec.size();
+		
+		return answer;
+	
+	}
+	
+	
+	//volume
+	Audio volume(std::pair<float, float> setting){
+		Audio volchange(*this);
+		
+		
+		
+		auto beg1 = volchange.audiovec.begin(), end1 = volchange.audiovec.end();
+		
+		while(beg1 != end1){
+		
+			float left = (*beg1).first * setting.first;
+			float right = (*beg1).second * setting.second;
+		
+		if (left > std::pow(2,bitnum-1)-1){
+				(*beg1).first = (std::pow(2,bitnum-1)-1);
+			}
+			else if(left < -1*std::pow(2,bitnum-1)-1){
+				(*beg1).first = -1*(std::pow(2,bitnum-1)-1);
+			}
+			else{
+				(*beg1).first = left;
+			}
+					
+		if (right > std::pow(2,bitnum-1)-1){
+				(*beg1).second = (std::pow(2,bitnum-1)-1);
+			}
+			else if(right < -1*std::pow(2,bitnum-1)-1){
+				(*beg1).second = -1*(std::pow(2,bitnum-1)-1);
+			}
+			else{
+				(*beg1).second = right;
+			}		
+					
+					
+		++beg1; 
+		}
+		std::cout <<"check " << volchange.audiovec[2].first <<" " << volchange.audiovec[2].second<<std::endl;
+		return volchange;
+		
+		
+	}
+	
+	//reverse
+	Audio reverse(){
+		Audio rev(*this);
+		std::cout <<"before working rev"<< rev.audiovec[0].first << " " << rev.audiovec[0].second<<std::endl;
+		std::reverse(rev.audiovec.begin(),rev.audiovec.end());
+		std::cout <<"working rev"<< rev.audiovec[3].first << " " << rev.audiovec[3].second<<std::endl;
+		return rev;
+	}
+	
+	
+	
+	
+	// RMS
+	std::pair<float, float> rms() {
+		
+		std::pair<int, int> sumpair = std::make_pair(0,0);
+
+		sumpair = std::accumulate(audiovec.begin(), audiovec.end(), sumpair, [](std::pair<int, int> a, std::pair<T, T> b) {
+			a.first = a.first + (b.first*b.first);
+			
+			a.second = a.second + (b.second*b.second);
+			return a;
+		});
+
+		float firstfloat = sqrt(sumpair.first / (float)samplenum);
+		
+		
+		float secondfloat = sqrt(sumpair.second / (float)samplenum);
+
+		std::pair<float, float> rmsanswer = std::make_pair(firstfloat, secondfloat);
+		
+		return rmsanswer;
+	}
+	
+		// normalisation
+	Audio normalisation(std::pair<float, float> givenrms) {
+		std::pair<float, float> nowrms = rms();
+
+		Audio answer( bitnum, fs);
+		answer.samplenum = samplenum;
+		if (bitnum == 8) {
+			std::transform(audiovec.begin(), audiovec.end(), answer.audiovec.begin(), normalise<int8_t, 2>(givenrms, nowrms, bitnum));
+		}
+		else if (bitnum == 16) {
+			
+			
+			std::transform(audiovec.begin(), audiovec.end(), answer.audiovec.begin(), normalise<int16_t, 2>(givenrms, nowrms, bitnum));
+		}
+
+		return answer;
+	}
 	
 	
 	//overloaded operators
-	
+	//add
 	Audio operator+(const Audio & other){
 	return add(other);
+	}
+		//cut
+	Audio operator^ (std::pair<int,int> cutrange){
+	return cut(cutrange);
+	}
+	//concatenate
+	Audio operator|( Audio & other){
+	return concatenate(other);
+	}
+	//volume
+	Audio operator* (std::pair<float,float> setting){
+		return volume(setting);
 	}
 };
 
--- Driver.cpp
+++ Driver.cpp
@@ -6,6 +6,7 @@ using namespace std;
 int main(int argc, char *argv[])
 {
 	
+	
 	int channelnum;
 		int bitsize;
 		int samplefreq;
@@ -41,9 +42,11 @@ int main(int argc, char *argv[])
 		
 
 		if(channelnum ==1){   // mono file
+		
 			cout<< "Mono" << endl;
 			
 			if(bitsize == 8){   //8 bit sample size
+			cout <<"8"<<endl;
 				DBRSAM003::Audio<int8_t,1> audio1(bitsize, samplefreq);
 				DBRSAM003::Audio<int8_t,1> audio2(bitsize, samplefreq);
 				
@@ -54,11 +57,12 @@ int main(int argc, char *argv[])
 				//add files together
 				DBRSAM003::Audio<int8_t,1> sum = audio1+audio2;
 				//save output
-				sum.save(string(argv[8]));
+				audio1.save(string(argv[8]));
 				
 			}
 			
 			else if(bitsize == 16){ //16 bit sample size
+			cout <<"16" << endl;
 				DBRSAM003::Audio<int16_t,1> audio1(bitsize, samplefreq);
 				DBRSAM003::Audio<int16_t,1> audio2(bitsize, samplefreq);
 				
@@ -66,9 +70,9 @@ int main(int argc, char *argv[])
 				audio1.load(string(argv[10]));
 				audio2.load(string(argv[11]));
 				//add files together
-				DBRSAM003::Audio<int16_t,1> sum = audio1+audio2;
+			//	DBRSAM003::Audio<int16_t,1> sum = audio1+audio2;
 				//save output
-				sum.save(string(argv[8]));
+				audio1.save(string(argv[8]));
 			}
 		}
 		
@@ -150,28 +154,498 @@ int main(int argc, char *argv[])
 			//loading file
 				audio1.load(string(argv[12]));
 			//cut
-		//	DBRSAM003::Audio<int8_t,2> cutaudio = audio1^cutrange;
+			DBRSAM003::Audio<int8_t,2> cutaudio = audio1^cutrange;
 				//save output
-		//	cutaudio.save(string(argv[8]));
+			cutaudio.save(string(argv[8]));
 			}
 		else if(bitsize == 16){ //16 bit sample size
 			DBRSAM003::Audio<int16_t,2> audio1(bitsize, samplefreq);
 			//loading file
-		//		audio1.load(string(argv[12]));
+				audio1.load(string(argv[12]));
 			//cut
-		//	DBRSAM003::Audio<int16_t,2> cutaudio = audio1^cutrange;
+			DBRSAM003::Audio<int16_t,2> cutaudio = audio1^cutrange;
 				//save output
-			//cutaudio.save(string(argv[8]));
+			cutaudio.save(string(argv[8]));
 			}
 		
 		}
 		
 	}
 	
+	//END OF CUT
+	
+	
+	//CONCATENATE
+	
+	else if (argc == 12 && string(argv[9]) == "-cat" ){  //check if concatenate command
+	cout << "Concatenating" << endl;
+	if(channelnum == 1){   // mono file
+			cout<< "Mono" << endl;
+			
+			if(bitsize == 8){   //8 bit sample size
+			DBRSAM003::Audio<int8_t,1> audio1(bitsize, samplefreq);
+			DBRSAM003::Audio<int8_t,1> audio2(bitsize, samplefreq);
+			//loading files
+				audio1.load(string(argv[10]));
+				audio2.load(string(argv[11]));
+			//concatenate
+			DBRSAM003::Audio<int8_t,1> concataudio = audio1|audio2;
+			//save output
+			concataudio.save(string(argv[8]));
+			}
+			
+			else if(bitsize == 16){ //16 bit sample size
+			DBRSAM003::Audio<int16_t,1> audio1(bitsize, samplefreq);
+			DBRSAM003::Audio<int16_t,1> audio2(bitsize, samplefreq);
+			//loading files
+				audio1.load(string(argv[10]));
+				audio2.load(string(argv[11]));
+			//cut
+			DBRSAM003::Audio<int16_t,1> concataudio = audio1|audio2;
+				//save output
+			concataudio.save(string(argv[8]));
+			}
+			
+		}
+		
+			else if(channelnum ==2){   //stereo file
+		cout << "Stereo" << endl;
+		if(bitsize == 8){   //8 bit sample size
+			DBRSAM003::Audio<int8_t,2> audio1(bitsize, samplefreq);
+			DBRSAM003::Audio<int8_t,2> audio2(bitsize, samplefreq);
+			//loading files
+				audio1.load(string(argv[10]));
+				audio2.load(string(argv[11]));
+			//concat
+			DBRSAM003::Audio<int8_t,2> concataudio = audio1|audio2;
+				//save output
+			concataudio.save(string(argv[8]));
+			}
+		else if(bitsize == 16){ //16 bit sample size
+			DBRSAM003::Audio<int16_t,2> audio1(bitsize, samplefreq);
+			DBRSAM003::Audio<int16_t,2> audio2(bitsize, samplefreq);
+			//loading files
+				audio1.load(string(argv[10]));
+				audio2.load(string(argv[11]));
+			//concat
+			DBRSAM003::Audio<int16_t,2> concataudio = audio1|audio2;
+				//save output
+			concataudio.save(string(argv[8]));
+			}
+		
+		}
+	
+	}
+	
+	
+	//Ranged add
+	
+		else if (argc == 16 && string(argv[9]) == "-radd" ){ 	//check if ranged command
+		cout << "Ranged Add" << endl;
+		int raddstart1;
+		int raddend1;
+		sstream << string(argv[10]);
+		sstream >> raddstart1;
+		sstream.clear();
+		sstream << string(argv[11]);
+		sstream >> raddend1;
+		sstream.clear();
+		std::pair<int,int> raddrange1 = std::make_pair(raddstart1,raddend1);
+		
+		int raddstart2;
+		int raddend2;
+		sstream << string(argv[12]);
+		sstream >> raddstart2;
+		sstream.clear();
+		sstream << string(argv[13]);
+		sstream >> raddend2;
+		sstream.clear();
+		std::pair<int,int> raddrange2 = std::make_pair(raddstart2,raddend2);
+	
+		
+		if(channelnum == 1){   // mono file
+			cout<< "Mono" << endl;
+			
+			if(bitsize == 8){   //8 bit sample size
+			
+			DBRSAM003::Audio<int8_t,1> audio1(bitsize, samplefreq);
+			DBRSAM003::Audio<int8_t,1> audio2(bitsize, samplefreq);
+			
+			//loading files
+				audio1.load(string(argv[14]));
+				audio2.load(string(argv[15]));
+				
+			//ranged add
+			DBRSAM003::Audio<int8_t,1> raddaudio = audio1.radd(raddrange1,raddrange2,audio2);
+			//save output
+			raddaudio.save(string(argv[8]));
+			}
+			
+			else if(bitsize == 16){ //16 bit sample size
+			DBRSAM003::Audio<int16_t,1> audio1(bitsize, samplefreq);
+			DBRSAM003::Audio<int16_t,1> audio2(bitsize, samplefreq);
+			//loading files
+				audio1.load(string(argv[14]));
+				audio2.load(string(argv[15]));
+			//ranged add
+			DBRSAM003::Audio<int16_t,1> raddaudio = audio1.radd(raddrange1, raddrange2, audio2);
+				//save output
+			raddaudio.save(string(argv[8]));
+			}
+			
+		}
+		
+			else if(channelnum ==2){   //stereo file
+		cout << "Stereo" << endl;
+		if(bitsize == 8){   //8 bit sample size
+			DBRSAM003::Audio<int8_t,2> audio1(bitsize, samplefreq);
+			DBRSAM003::Audio<int8_t,2> audio2(bitsize, samplefreq);
+			//loading files
+				audio1.load(string(argv[14]));
+				audio2.load(string(argv[15]));
+			//ranged add
+			DBRSAM003::Audio<int8_t,2> raddaudio = audio1.radd(raddrange1, raddrange2, audio2);
+				//save output
+			raddaudio.save(string(argv[8]));
+			}
+		else if(bitsize == 16){ //16 bit sample size
+			DBRSAM003::Audio<int16_t,2> audio1(bitsize, samplefreq);
+			DBRSAM003::Audio<int16_t,2> audio2(bitsize, samplefreq);
+			//loading files
+				audio1.load(string(argv[14]));
+				audio2.load(string(argv[15]));
+			//ranged add
+			DBRSAM003::Audio<int16_t,2> raddaudio = audio1.radd(raddrange1, raddrange2, audio2);
+				//save output
+			raddaudio.save(string(argv[8]));
+			}
+		
+		}
+		
+		
+		}
+	
+	
+	//volume
+	else if (argc == 13 && string(argv[9]) == "-v" ){ 	//check if volume command
+		cout << "Volume" << endl;
+		float leftvol;
+		float rightvol;
+		sstream << string(argv[10]);
+		sstream >> leftvol;
+		sstream.clear();
+		sstream << string(argv[11]);
+		sstream >> rightvol;
+		sstream.clear();
+		
+		std::pair<float,float> volpair = std::make_pair(leftvol,rightvol);
+		cout << volpair.first << " " <<volpair.second << endl;
+		
+		if(channelnum == 1){   // mono file
+			cout<< "Mono" << endl;
+			
+			if(bitsize == 8){   //8 bit sample size
+			
+			DBRSAM003::Audio<int8_t,1> audio1(bitsize, samplefreq);
+			
+			
+			//loading files
+				audio1.load(string(argv[12]));
+				
+				
+			//volume
+			DBRSAM003::Audio<int8_t,1> volaudio = audio1*(volpair);
+			//save output
+			volaudio.save(string(argv[8]));
+			}
+			
+			else if(bitsize == 16){ //16 bit sample size
+			DBRSAM003::Audio<int16_t,1> audio1(bitsize, samplefreq);
+			
+			//loading files
+				audio1.load(string(argv[12]));
+				
+			//volume
+			DBRSAM003::Audio<int16_t,1> volaudio = audio1*(volpair);
+				//save output
+			volaudio.save(string(argv[8]));
+			}
+			
+		}
+		
+			else if(channelnum ==2){   //stereo file
+		cout << "Stereo" << endl;
+		if(bitsize == 8){   //8 bit sample size
+			DBRSAM003::Audio<int8_t,2> audio1(bitsize, samplefreq);
+			
+			//loading files
+				audio1.load(string(argv[12]));
+				
+			//volume
+			DBRSAM003::Audio<int8_t,2> volaudio =  audio1*(volpair);
+				//save output
+			volaudio.save(string(argv[8]));
+			}
+		else if(bitsize == 16){ //16 bit sample size
+			DBRSAM003::Audio<int16_t,2> audio1(bitsize, samplefreq);
+			
+			//loading files
+				audio1.load(string(argv[12]));
+				
+			//ranged add
+			DBRSAM003::Audio<int16_t,2> volaudio =  audio1*(volpair);
+				//save output
+			volaudio.save(string(argv[8]));
+			}
+		
+		}
+		
+		
+		}
+		
+		
+		//reverse
+	else if (argc == 11 && string(argv[9]) == "-rev" ){ 	//check if reverse command
+		cout << "Reverse" << endl;
+		
+		
+		
+		
+		if(channelnum == 1){   // mono file
+			cout<< "Mono" << endl;
+			
+			if(bitsize == 8){   //8 bit sample size
+			
+			DBRSAM003::Audio<int8_t,1> audio1(bitsize, samplefreq);
+			
+			
+			//loading files
+				audio1.load(string(argv[10]));
+				
+				
+			//reverse
+			DBRSAM003::Audio<int8_t,1> revaudio = audio1.reverse();
+			//save output
+			revaudio.save(string(argv[8]));
+			}
+			
+			else if(bitsize == 16){ //16 bit sample size
+			DBRSAM003::Audio<int16_t,1> audio1(bitsize, samplefreq);
+			
+			//loading files
+				audio1.load(string(argv[10]));
+				
+			//reverse
+			DBRSAM003::Audio<int16_t,1> revaudio = audio1.reverse();
+				//save output
+			revaudio.save(string(argv[8]));
+			}
+			
+		}
+		
+			else if(channelnum ==2){   //stereo file
+		cout << "Stereo" << endl;
+		if(bitsize == 8){   //8 bit sample size
+			DBRSAM003::Audio<int8_t,2> audio1(bitsize, samplefreq);
+			
+			//loading files
+				audio1.load(string(argv[10]));
+				
+			//reverse
+			DBRSAM003::Audio<int8_t,2> revaudio = audio1.reverse();
+				//save output
+			revaudio.save(string(argv[8]));
+			}
+		else if(bitsize == 16){ //16 bit sample size
+			DBRSAM003::Audio<int16_t,2> audio1(bitsize, samplefreq);
+			
+			//loading files
+				audio1.load(string(argv[10]));
+				
+			//reverse
+			DBRSAM003::Audio<int16_t,2> revaudio = audio1.reverse();
+				//save output
+			revaudio.save(string(argv[8]));
+			}
+		
+		}
+		
+		
+		}
+		
+		//rms
+	else if (argc == 11 && string(argv[9]) == "-rms" ){ 	//check if reverse command
+		cout << "RMS" << endl;
+		
+		
+		
+		
+		if(channelnum == 1){   // mono file
+			cout<< "Mono" << endl;
+			
+			if(bitsize == 8){   //8 bit sample size
+			
+			DBRSAM003::Audio<int8_t,1> audio1(bitsize, samplefreq);
+			
+			
+			//loading files
+				audio1.load(string(argv[10]));
+				
+				
+			//RMS
+			cout << "RMS for channel 1 is " << audio1.rms()<<endl;
+			
+			
+			}
+			
+			else if(bitsize == 16){ //16 bit sample size
+			DBRSAM003::Audio<int16_t,1> audio1(bitsize, samplefreq);
+			
+			//loading files
+				audio1.load(string(argv[10]));
+				
+			//RMS
+			cout << "RMS for channel 1 is " << audio1.rms()<<endl;
+			
+			
+				
+			}
+			
+		}
+		
+			else if(channelnum ==2){   //stereo file
+		cout << "Stereo" << endl;
+		if(bitsize == 8){   //8 bit sample size
+			DBRSAM003::Audio<int8_t,2> audio1(bitsize, samplefreq);
+			
+			//loading files
+				audio1.load(string(argv[10]));
+				
+			//RMS
+			cout << "RMS for channel 1 is " << audio1.rms().first<<endl;
+			cout << "RMS for channel 2 is " << audio1.rms().second<<endl;
+			
+			}
+		else if(bitsize == 16){ //16 bit sample size
+			DBRSAM003::Audio<int16_t,2> audio1(bitsize, samplefreq);
+			
+			//loading files
+				audio1.load(string(argv[10]));
+				
+			//RMS
+			cout << "RMS for channel 1 is " << audio1.rms().first<<endl;
+			cout << "RMS for channel 2 is " << audio1.rms().second<<endl;
+			
+			}
+		
+		}
+		
+		
+		}
+		
+		
+		//normalise
+		else if (argc == 13 && string(argv[9]) == "-norm" ){ 	//check if normalise command
+		cout << "Normalise" << endl;
+		float rms1;
+		float rms2;
+		sstream << string(argv[10]);
+		sstream >> rms1;
+		sstream.clear();
+		sstream << string(argv[11]);
+		sstream >> rms2;
+		sstream.clear();
+		std::pair<float,float> normpair = std::make_pair(rms1,rms2);
+		
+		
+		
+		if(channelnum == 1){   // mono file
+			cout<< "Mono" << endl;
+			
+			if(bitsize == 8){   //8 bit sample size
+			
+			DBRSAM003::Audio<int8_t,1> audio1(bitsize, samplefreq);
+			
+			
+			//loading files
+				audio1.load(string(argv[10]));
+				
+				
+			//normalise
+			DBRSAM003::Audio<int8_t,1> normaudio = audio1.normalisation(normpair);
+			//save
+			normaudio.save(string(argv[8]));
+			}
+			
+			else if(bitsize == 16){ //16 bit sample size
+			DBRSAM003::Audio<int16_t,1> audio1(bitsize, samplefreq);
+			
+			//loading files
+				audio1.load(string(argv[10]));
+				
+			//normalise
+			DBRSAM003::Audio<int16_t,1> normaudio = audio1.normalisation(normpair);
+			//save
+			normaudio.save(string(argv[8]));
+			
+			
+				
+			}
+			
+		}
+		
+			else if(channelnum ==2){   //stereo file
+		cout << "Stereo" << endl;
+		if(bitsize == 8){   //8 bit sample size
+			DBRSAM003::Audio<int8_t,2> audio1(bitsize, samplefreq);
+			
+			//loading files
+				audio1.load(string(argv[10]));
+				
+		
+				//normalise
+			DBRSAM003::Audio<int8_t,2> normaudio = audio1.normalisation(normpair);
+			//save
+			normaudio.save(string(argv[8]));
+			
+			}
+		else if(bitsize == 16){ //16 bit sample size
+			DBRSAM003::Audio<int16_t,2> audio1(bitsize, samplefreq);
+			
+			//loading files
+				audio1.load(string(argv[10]));
+				
+			//normalise
+			DBRSAM003::Audio<int16_t,2> normaudio = audio1.normalisation(normpair);
+			//save
+			normaudio.save(string(argv[8]));
+			
+			}
+		
+		}
+		
+		
+		}
 	
 	
 	
 	
-	
-
+				
 } 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
